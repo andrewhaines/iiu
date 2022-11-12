33f4c6877054052f2ca5d59c45ada9b343ce85e1
andrewhaines <andrew.m.haines@gmail.com>
2022-11-12 10:27:01 -0500

    Update homepage language

--- src/assets/data/homePage.js
+++ src/assets/data/homePage.js
@@ -9,13 +9,19 @@
 export const companyCards = [
   {
     heading: 'IIU Advisors',
-    description: 'Premier mergers and acquisitions advisory business focused on the middle market in Virginia and the Mid-Atlantic states.',
+    description: 'IIU Advisors is a premier mergers and acquisitions advisory firm focused on the middle market in Virginia and the Mid-Atlantic states &mdash; with over 50 years experience buying and selling operating businesses.',
     linkText: 'Visit website',
     link: '/advisors'
   },
   {
+    heading: 'IIU Financing',
+    description: 'IIU Financing sources and structures financing to optimize transactions, working with equity investors, financial institutions and private credit funds to ensure a successful transaction or recapitalization.',
+    linkText: 'Visit website',
+    link: '#'
+  },
+  {
     heading: 'Wallach & Company, Inc.',
-    description: 'For more than 30 years, Wallach has provided medical and related insurance to people traveling outside their home countries for work, study, or vacation.',
+    description: 'For more than 30 years, Wallach & Company has provided medical and related insurance to people traveling outside their home countries for work, study, or vacation.',
     linkText: 'Visit website',
     link: 'https://wallach.com'
   }
