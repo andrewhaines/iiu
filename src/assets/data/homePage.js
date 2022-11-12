a099f7cb9dcb3f7f910a8362542cabe7bcbac896
andrewhaines <andrew.m.haines@gmail.com>
2022-11-12 09:50:36 -0500

    Update homepage companies

--- src/assets/data/homePage.js
+++ src/assets/data/homePage.js
@@ -1,7 +1,22 @@
 // Home page card data
-export const directusData = async function() {
-  const request = await fetch('https://iiu.directus.app/items/home_page');
-  const response = await request.json();
-  const data = await response.data.cards
-  return data;
-}
+// export const directusData = async function() {
+//   const request = await fetch('https://iiu.directus.app/items/home_page');
+//   const response = await request.json();
+//   const data = await response.data.cards
+//   return data;
+// }
+
+export const companyCards = [
+  {
+    heading: 'IIU Advisors',
+    description: 'Premier mergers and acquisitions advisory business focused on the middle market in Virginia and the Mid-Atlantic states.',
+    linkText: 'Visit website',
+    link: '/advisors'
+  },
+  {
+    heading: 'Wallach & Company, Inc.',
+    description: 'For more than 30 years, Wallach has provided medical and related insurance to people traveling outside their home countries for work, study, or vacation.',
+    linkText: 'Visit website',
+    link: 'https://wallach.com'
+  }
+]
