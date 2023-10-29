6f94bfd544b663416b1f5c1a1b5f1944be2770ed
andrewhaines <andrew.m.haines@gmail.com>
2023-10-28 23:04:26 -0400

    Update financing language

--- src/assets/data/financingPage.js
+++ src/assets/data/financingPage.js
@@ -1,30 +1,30 @@
 export const servicesCards = [
   {
-    heading: 'Equity Capital Injections ',
-    description: ''
+    heading: 'Equity Capital Injections',
+    description: 'We often invest in the businesses we advise. We prefer to have skin in the game before we encourage any third party to invest. When a company is in need of additional capital and debt is inadvisable or unavailable, an equity investment is often the best option.<br><br>If a company is interested in raising equity capital we are available for a no risk consultation. Our <a href="/equity" class="text-sky-700">equity business</a> may be interested in investing and/or we can introduce other investors which may be interested in an ownership interest on terms beneficial to existing shareholders and new investors alike.'
   },
   {
     heading: 'Preferred Equity',
-    description: '',
+    description: 'Preferred equity is considered a hybrid security because it often carries the characteristics of both debt and equity. It is an equity ownership interest in a company with specific and often limited rights in relation to common equity. It often carries a preferred dividend which must be satisfied prior to any distributions to common equity.<br><br>We work with companies to structure the right solution which benefits the company, its shareholders and new investors. Often preferred equity is an ideal structure for companies in need of capital but concerned about ceding control to new investors or taking on excessive debt liabilities.',
   },
   {
     heading: 'Mezzanine Debt',
-    description: ''
+    description: 'Mezzanine debt is similar to preferred equity because it often combines the characteristics of debt and equity. It is a binding debt obligation of the company and senior to any equity securities in insolvency or liquidation proceedings<br><br> Mezzanine securities often have a convertibility feature which allows for the debt securities to be exchanged or “converted” to equity securities under certain pre-negotiated circumstances.<br><br>Like preferred equity, mezzanine debt securities are often ideal when willing to pay a preferred return in exchange for ceding less control yet with less onerous obligations than traditional debt.'
   },
   {
     heading: 'Bridge Financing',
-    description: ''
+    description: 'A “bridge loan” is a relatively short duration debt security designed to provide the borrower with capital for a finite purpose and time period. These loans are often an ideal solution for companies or individuals who are actively creating value in a project and need capital rapidly.<br><br>Bridge loans are often used when traditional debt financing is either unavailable or inadvisable.<br><br>Bridge financing is often expensive and comes with strong covenants designed to protect the borrower in exchange for the rapid deployment of capital. We work with companies to determine if bridge financing makes sense for their desired outcome and importantly if the rewards justify the risk.'
   },
   {
-    heading: 'Lines of Credit',
-    description: ''
+    heading: 'Lines of Credit (LOC)',
+    description: 'A line of credit is a debt facility designed to provide liquidity to operating businesses. Most often these lines of credit will be “revolving” or “self-liquidating”. Lines of credit are often used to grow the activities of a business through additional sales or increasing inventory.<br><br>We work with clients to ensure they are in a position to effectively use a LOC without endangering their business or violating the covenants of any other debt that may be senior to the LOC.'
   },
   {
     heading: 'Trade & Receivable Financing',
-    description: ''
+    description: 'Oftentimes a profitable business simply runs low on cash needed to meet current obligations. This could be as a result of cash flow seasonality, sales growth that outpaces receivable collections and unexpected delays in the supply chain.<br><br>We work with clients to provide debt solutions to cross the “cash gap” without endangering the business or violating any covenants of existing obligations.'
   },
   {
     heading: 'Equipment Financing',
-    description: ''
+    description: 'Upgrading or replacing equipment can be a financial burden and greatly impact the cash flow of a profitable business.<br><br>We work with companies to structure the financing of critical equipment without hampering the current performance or future growth of a business.'
   }
 ]
